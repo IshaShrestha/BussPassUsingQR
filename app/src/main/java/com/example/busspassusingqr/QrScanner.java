a902d08b7a54983f3475b8656d8d953e240cb1e1
Isha Shrestha <eeshashrestha18@gmail.com>
2020-07-01 20:52:22 +0545

    scan1

--- app/src/main/java/com/example/busspassusingqr/QrScanner.java
+++ app/src/main/java/com/example/busspassusingqr/QrScanner.java
@@ -2,57 +2,89 @@ package com.example.busspassusingqr;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Point;
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
-import com.google.zxing.integration.android.IntentIntegrator;
-import com.google.zxing.integration.android.IntentResult;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcode;
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcodeDetector;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
 
-public class QrScanner extends AppCompatActivity{
-    private Button btn_scanner;
+import java.util.List;
+
+
+public class QrScanner extends AppCompatActivity {
+    private Button btn_scan;
+    private final int BARCODE_RECO_REQ_CODE=200;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scanner);
-        btn_scanner= findViewById(R.id.btn_scanner);
-
-        btn_scanner.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                IntentIntegrator intentIntegrator = new IntentIntegrator(QrScanner.this);
-                intentIntegrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
-                intentIntegrator.setCameraId(0);
-                intentIntegrator.setOrientationLocked(false);
-                intentIntegrator.setPrompt("scanning");
-                intentIntegrator.setBeepEnabled(true);
-                intentIntegrator.setBarcodeImageEnabled(true);
-                intentIntegrator.initiateScan();
-
-
-            }
-        }
-        );
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        IntentResult result = IntentIntegrator.parseActivityResult(requestCode,resultCode,data);
-        if (result!=null && result.getContents()!=null){
-            new AlertDialog.Builder(QrScanner.this)
-                    .setTitle("Scan result")
-                    .setMessage(result.getContents())
-                    .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int i) {
-                            dialog.dismiss();
-                        }
-                    }) .create() .show();
-
-        }
         super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode==BARCODE_RECO_REQ_CODE) {
+            if (resultCode==RESULT_OK) {
+                Bitmap photo=(Bitmap)data.getExtras().get("data");
+                barcodeRecognition(photo);
+            }
+        }
     }
+
+    private void barcodeRecognition(Bitmap photo) {
+        FirebaseVisionImage image= FirebaseVisionImage.fromBitmap(photo);
+        FirebaseVisionBarcodeDetector detector = FirebaseVision.getInstance().getVisionBarcodeDetector();
+        Task<List<FirebaseVisionBarcode>> result = detector.detectInImage(image).addOnSuccessListener(new OnSuccessListener<List<FirebaseVisionBarcode>>() {
+            @Override
+            public void onSuccess(List<FirebaseVisionBarcode> barcodes) {
+                for (FirebaseVisionBarcode barcode: barcodes) {
+                    Rect bounds = barcode.getBoundingBox();
+                    Point[] corners = barcode.getCornerPoints();
+
+                    String rawValue = barcode.getRawValue();
+
+                    int valueType = barcode.getValueType();
+                    Toast.makeText(QrScanner.this, rawValue, Toast.LENGTH_SHORT).show();
+                }
+
+
+            }
+        })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(QrScanner.this,"Something went wrong", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+    }
+
+
+    public void barcodeReco(View view) {
+        Intent intent=new Intent (MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent,BARCODE_RECO_REQ_CODE);
+
+    }
+
+
+
 }
+
+
+
--- app/src/main/java/com/example/busspassusingqr/Register.java
+++
@@ -1,7 +0,0 @@
-package com.example.busspassusingqr;
-
-import android.support.v7.app.AppCompatActivity;
-
-public class Register extends AppCompatActivity {
-
-}
