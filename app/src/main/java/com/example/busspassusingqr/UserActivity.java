72178df0e48242cd669c68d111d36fce5e517084
Isha Shrestha <eeshashrestha18@gmail.com>
2020-07-16 17:42:31 +0545

    Bus

--- app/src/main/java/com/example/busspassusingqr/UserActivity.java
+++ app/src/main/java/com/example/busspassusingqr/UserActivity.java
@@ -17,10 +17,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
-import com.khalti.checkout.helper.OnCheckOutListener;
-import com.khalti.checkout.helper.PaymentPreference;
-import com.khalti.utils.Constant;
-import com.khalti.widget.KhaltiButton;
+
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -150,32 +147,6 @@ public class UserActivity extends AppCompatActivity {
             }
         });
 
-        KhaltiButton khaltiButton = (KhaltiButton) findViewById(R.id.khalti_button);
-
-        Map<String, Object> map = new HashMap<>();
-        map.put("merchant_extra", "This is extra data");
-
-        Builder builder = new Config.Builder(Constant.pub, "Product ID", "Main", 1100L, new OnCheckOutListener() {
-            @Override
-            public void onError(@NonNull String action, @NonNull Map<String, String> errorMap) {
-                Log.i(action, errorMap.toString());
-            }
-
-            @Override
-            public void onSuccess(@NonNull Map<String, Object> data) {
-                Log.i("success", data.toString());
-            }
-        })
-                .paymentPreferences(new ArrayList<PaymentPreference>() {{
-                    add(PaymentPreference.KHALTI);
-                    add(PaymentPreference.EBANKING);
-                    add(PaymentPreference.MOBILE_BANKING);
-                    add(PaymentPreference.CONNECT_IPS);
-                    add(PaymentPreference.SCT);
-                }})
-                .additionalData(map)
-                .productUrl("http://example.com/product")
-                .mobile("9800000000");
 
 
 
